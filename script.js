e7fbf4b4b07155dd1485464c190f462b2416f047
lance Watson <lancechise@icloud.cominitgit initgit statuslscd /users/lance/Desktop/watson-part-two/pwd>
2022-12-14 07:37:30 -0500

    part two golf

---
+++ script.js
@@ -0,0 +1,39 @@
+let player=prompt("Welcome to GC mini golf! What is your name? ");
+let holes=prompt("Hi, " +  player  + " ! Would you like to play 3 or 6 holes today?");
+let total= 0 ;
+for(let i= 0; i < holes; i++){
+    let par=Number(prompt("how many puts for hole " + i + "?") );
+    total += par;
+}
+
+
+
+if( holes ==3)
+  if(total > 9 ) {
+    console.log("Nice try," +  player  + "  your  par was + " + total + " ");}
+
+    if( holes ==3)
+      if (total <= 6 ) {
+      console.log("Great Job," +  player  + "  your  par was -  " + total + " ");}
+
+      if( holes ==3)
+       if (total <= 3 ) {
+      console.log("Good game," +  player  + "  your  par was -  " + total + " ");}
+    
+
+      if( holes ==6)
+       if(total > 18 ) {
+      console.log("Nice try," +  player  + "  your  par was + " + total + " ");}
+
+    if( holes ==6)
+      if (total <= 12 ) {
+      console.log("Great Job," +  player  + "  your  par was -  " + total + " ");}
+
+      if( holes ==6)
+        if (total <= 6 ) {
+      console.log("Good game," +  player  + "  your  par was -  " + total + " ");}
+    
+
+
+
+   
