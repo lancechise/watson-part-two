0e12ece83a1036d78267d21c6a16160d1b924fdf
lance Watson <lancechise@icloud.cominitgit initgit statuslscd /users/lance/Desktop/watson-part-two/pwd>
2022-12-15 14:27:17 -0500

    projrct 2 updated

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 let player=prompt("Welcome to GC mini golf! What is your name? ");
 let holes=prompt("Hi, " +  player  + " ! Would you like to play 3 or 6 holes today?");
 let total= 0 ;
-for(let i= 0; i < holes; i++){
+for(let i= 1; i <= holes; i++){
     let par=Number(prompt("how many puts for hole " + i + "?") );
-    total += par;
+    
+    
+    total += par ;
+    
 }
 
 
 
-if( holes ==3)
-  if(total > 9 ) {
-    console.log("Nice try," +  player  + "  your  par was + " + total + " ");}
+console.log("par is + " + total + "." );
+ 
 
-    if( holes ==3)
-      if (total <= 6 ) {
+
+
+if( holes ==3 && total >= 9 ){
+    console.log ("Nice try," +  player  + "  your  par was + " + total + ".");}
+    if( holes ==3 && total <= 6){
+      console.log ("Great Job, " +  player  + "  your  par was -  " + total + ".");}
+      if( holes ==3 && total < 3){
+        console.log ("Good game, " +  player  + "  your  par was   " + total + ".");}
+
+        if( holes ==6 && total >= 18 ){
+          console.log ("Nice try," +  player  + "  your  par was + " + total + ".");}
+          if( holes ==6 && total >= 12){
+            console.log ("Great Job, " +  player  + "  your  par was -  " + total + ".");}
+            if( holes ==6 && total <= 6){
+              console.log ("Good game, " +  player  +  "  your  par was + " + total + ".");}
+      
+
+
+
+
+
+//if( holes ==3)
+ // if(total > 9 ) {
+   // console.log("Nice try," +  player  + "  your  par was + " + total + " ");}
+
+    /* if( holes ==3)
+       if(total <= 6 ) {
       console.log("Great Job," +  player  + "  your  par was -  " + total + " ");}
 
       if( holes ==3)
@@ -33,7 +60,7 @@ if( holes ==3)
         if (total <= 6 ) {
       console.log("Good game," +  player  + "  your  par was -  " + total + " ");}
     
-
+     */
 
 
    
