be3256e68e1ca3292fc9d3af85ea058b0274cc18
lance Watson <lancechise@icloud.cominitgit initgit statuslscd /users/lance/Desktop/watson-part-two/pwd>
2022-12-15 21:42:18 -0500

    golf part two updated

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+
 let player=prompt("Welcome to GC mini golf! What is your name? ");
 let holes=prompt("Hi, " +  player  + " ! Would you like to play 3 or 6 holes today?");
-let total= 0 ;
+let total = 0 ;
 for(let i= 1; i <= holes; i++){
     let par=Number(prompt("how many puts for hole " + i + "?") );
-    
+    console.log("par is + " + total + "." );
     
     total += par ;
     
